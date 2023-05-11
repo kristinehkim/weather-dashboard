08c75be983bf3a4a794847dbf9edd7db28fafe4c
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-05-10 17:39:31 -0700

    working on 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ function onSearch(event) {
 }
 // taking the cityName the user inputs and getting the lat and lon
 function fetchCoords(cityName) {
-    let requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${anotherKey}`//template literals or template string - ${variable}, the whole thing in back ticks ` ` (grave tick)
+    let requestUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=${anotherKey}`//template literals or template string - ${variable}, the whole thing in back ticks ` ` (grave tick)
     return fetch(requestUrl).then(function(response) {
         return response.json()
     }).then(function(data) {
@@ -27,45 +27,60 @@ function fetchCoords(cityName) {
     // console.log(typeof data[0])
     const lon = data[0].lon; 
     // console.log(lat, lon)
+    getForecast(lat, lon);
     return {
         lat: lat, 
         lon: lon,   
     }
-  
     })
-    
 };
 
 // google "how to change asynch await to fetch"
 
-function getWeather(lat, lon) { // defining properties make sure they match lat and lon match in the url below, defining function here (define functions once)
-    let requestUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${anotherKey}`//let you can use the same variable name, var you have to use a different variable name
-    fetch(requestUrl).then(function(response) {
-        return response.json()
-    }).then (function(data) {
-        // console.log(data);
-    // const wind = data.wind.speed; //grabbing the properties stored in an object
-    // const temp = data.main.temp;
-    // const humidity = data.main.humidity;
-    // console.log(wind, temp, humidity);
-    getWeather(lat, lon);
-    getForecast(lat, lon);
-    // return {
-    //     wind: wind,
-    //     temp: temp,
-    //     humidity: humidity
-    // }
-})
+// function getWeather(lat, lon) { // defining properties make sure they match lat and lon match in the url below, defining function here (define functions once)
+//     let requestUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${anotherKey}`//let you can use the same variable name, var you have to use a different variable name
+//     fetch(requestUrl).then(function(response) {
+//         return response.json()
+//     }).then (function(data) {
+//         console.log(data);
+//     const wind = data.wind.speed; //grabbing the properties stored in an object
+//     const temp = data.main.temp;
+//     const humidity = data.main.humidity;
+//     console.log(wind, temp, humidity);
+    
+//     // getForecast(lat, lon);
+//     return {
+//         wind: wind,
+//         temp: temp,
+//         humidity: humidity
+//     }
+// })
     // getWeather(lat, lon); //call function getWeather because lat and lon live here, lat and lon are the arguments, you can call functions an infinite amount of times 
-};
+// };
 
 function getForecast(lat, lon) {
-    let requestUrl = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${anotherKey}`
+    let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${lat}&lon=${lon}&appid=${anotherKey}`
     fetch(requestUrl).then(function(response) {
         return response.json()
     }).then (function(data) {
         console.log(data);
+        if (data.list[0].dt_txt == '03:00:00') {
+            const temp = data.list[0].main.temp;
+            const wind = data.list[0].wind.speed;
+            const humidity = data.list[0].main.humidity;
+            console.log(temp, wind, humidity);
+            return {
+                temp: temp,
+                wind: wind,
+                humidity: humidity
+            }
+        }
+        // for (var i = 0; i < data.list.length; i++) {
+        //     printResults(locRes.results[i]);//checking the time dt
     })
+
+    //  if dt... is 1:00 then get the data for that item for that specific time and render a forecast card for it (for loop)
+    // function that creates html elements for each piece of weather data that we want
 };
 
 //use js or jQuery to build the cards within the functions
