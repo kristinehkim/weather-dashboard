f6214cc5d6ca9e246f62553efebb3cccdb07835e
kristinehkim <122055613+kristinehkim@users.noreply.github.com>
2023-05-11 13:34:45 -0700

    cards created, README added

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-# weather-dashboard
+# PWeather Dashboard
+> This is a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.  The 5 Day Weather Forecast on OpenWeather was used to retrieve the weather data for searched cities.  An API key was generated to retrieve the data.
+> Live demo [_here_](https://kristinehkim.github.io/weather-dashboard/). <!-- If you have the project hosted somewhere, include the link here. -->
+
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Features](#features)
+* [Screenshots](#screenshots)
+* [Setup](#setup)
+* [Usage](#usage)
+* [Project Status](#project-status)
+* [Room for Improvement](#room-for-improvement)
+* [Acknowledgements](#acknowledgements)
+* [Contact](#contact)
+<!-- * [License](#license) -->
+
+
+## General Information
+The user will choose a particular city to find weather information.  Once a city is typed in the search box, the current weather and a 5 day forecast of the weather for that city will be displayed.  For each day, the temperature, wind speed, and humidity will be displayed, as well.
+<!-- You don't have to answer all the questions - just the ones relevant to your project. -->
+
+
+## Technologies Used
+- OpenWeather
+- Postman API Platform
+
+
+## Features
+- Search for weather of chosen city
+- Displays current weather
+- Displays a 5 day forecast of weather
+
+
+## Screenshots
+![Example screenshot](./img/screenshot.png)
+<!-- If you have screenshots you'd like to share, include them here. -->
+
+
+## Setup
+What are the project requirements/dependencies? Where are they listed? A requirements.txt or a Pipfile.lock file perhaps? Where is it located?
+
+Proceed to describe how to install / setup one's local environment / get started with the project.
+
+
+## Usage
+The user will type a city name in the search box.  Once the search button is clicked, it will display current weather and a 5 day forecast.
+
+
+## Project Status
+Project is: _in progress_.
+
+
+## Room for Improvement
+Include areas you believe need improvement / could be improved. Also add TODOs for future development.
+
+Room for improvement:
+- Improvement to be done 1
+- Improvement to be done 2
+
+To do:
+- Feature to be added 1
+- Feature to be added 2
+
+
+## Acknowledgements
+- Many thanks to my instuctor, Michael Pacheco, the TAs, Nirav Venkatesan and Julius Dorfman, and tutor, Dane Shrewsbury. Also, to all the Ask BCS people that helped me!
+
+
+## Contact
+Created by Kristine Kim.  Feel free to email me at kristinehkim@gmail.com. 
+
+
+<!-- Optional -->
+<!-- ## License -->
+<!-- This project is open source and available under the [... License](). -->
+
+<!-- You don't have to include all sections - just the one's relevant to your project -->
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 // var weatherApiKey = "2ab608278c156bf43d29f7d1d05beaaf";
 let anotherKey = "cb2968fd932e7bdc026f52918804b584";
 let forecastContainer = document.querySelector('#forecast-container');
-// var city;
-
-// var queryString =
-
+let weatherContainer = document.querySelector('#weather-container');
 let searchButton = document.querySelector('#search-button');
 let searchInput = document.querySelector('#search');
+
+let currentDay = dayjs().format('dddd, MM/DD/YYYY');
 
 // onSearch is an eventListener that is grabbing the searchValue, and passing it to the fetchCoords function
 function onSearch(event) {
     // assigning a variable searchValue to what the user inputs searchInput.value, searchValue is grabbing the city the user inputs
-    var searchValue = searchInput.value;
+    let searchValue = searchInput.value;
     // var searchResults = '/forecast?'
-
+    localStorage.setItem('city', searchValue);
     // fetchCoords is being called, this returns the lat and lon data
     fetchCoords(searchValue);
 }
@@ -36,7 +35,6 @@ function fetchCoords(cityName) {
     })
 };
 
-// google "how to change asynch await to fetch"
 
 function getWeather(lat, lon) { // defining properties make sure they match lat and lon match in the url below, defining function here (define functions once)
     let requestUrl = `https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=${lat}&lon=${lon}&appid=${anotherKey}`//let you can use the same variable name, var you have to use a different variable name
@@ -44,21 +42,36 @@ function getWeather(lat, lon) { // defining properties make sure they match lat 
         return response.json()
     }).then(function (data) {
         // console.log(data);
-        const temp = data.main.temp;
-        const wind = data.wind.speed; //grabbing the properties stored in an object
-        const humidity = data.main.humidity;
-        console.log(wind, temp, humidity);
+        const div = document.createElement('div');
+        const city = document.createElement('h2'); 
+        const date = document.createElement('h2');
+        const temp = document.createElement('h3');
+        const wind = document.createElement('h3');
+        const humidity = document.createElement('h3');
+
+        div.classList = 'card'
+        city.innerText = `${searchInput.value}`
+        date.innerText = `${currentDay}`
+        temp.innerText = `Temp: ${data.main.temp}`
+        wind.innerText = `Wind: ${data.wind.speed}`
+        humidity.innerText = `Humidity: ${data.main.humidity}`
+
+        div.appendChild(city);
+        div.appendChild(date);
+        div.appendChild(temp);
+        div.appendChild(wind);
+        div.appendChild(humidity);
+        weatherContainer.appendChild(div);
 
         getForecast(lat, lon);
-        return {
-            wind: wind,
-            temp: temp,
-            humidity: humidity
-        }
+        // return {
+        //     wind: wind,
+        //     temp: temp,
+        //     humidity: humidity
+        // }
     })
 };
-// getWeather(lat, lon); //call function getWeather because lat and lon live here, lat and lon are the arguments, you can call functions an infinite amount of times 
-// };
+
 
 function getForecast(lat, lon) {
     let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=${lat}&lon=${lon}&appid=${anotherKey}`
@@ -68,69 +81,27 @@ function getForecast(lat, lon) {
         // console.log(data);//returns an array of 40 objects breaking down the 5 days into 8 3 hour blocks
         let fiveDayArr = data.list.filter(forecast => forecast.dt_txt.includes("12:00:00"));
         console.log(fiveDayArr);
-        fiveDayArr.forEach(fiveDayArr => {
+        fiveDayArr.forEach(data => {//forEach going through the data of the fiveDayArr
             const div = document.createElement('div');
+            const date = document.createElement('h2');
             const temp = document.createElement('h3');
             const wind = document.createElement('h3');
             const humidity = document.createElement('h3');
 
             div.classList = 'card'
+            date.innerText = `${currentDay}`
             temp.innerText = `Temp: ${data.main.temp}`
             wind.innerText = `Wind: ${data.wind.speed}`
             humidity.innerText = `Humidity: ${data.main.humidity}`
 
-            div.appendChild(temp)
-            div.appendChild(wind)
-            div.appendChild(humidity)
+            div.appendChild(date);
+            div.appendChild(temp);
+            div.appendChild(wind);
+            div.appendChild(humidity);
+            forecastContainer.appendChild(div);//appending the temp, wind, humidity to the forecast container
         });
     })
 }
-// if (data.list[0].dt_txt == '03:00:00') {
-//     const temp = data.list[0].main.temp;
-//     const wind = data.list[0].wind.speed;
-//     const humidity = data.list[0].main.humidity;
-//     console.log(temp, wind, humidity);
-//     return {
-//         temp: temp,
-//         wind: wind,
-//         humidity: humidity
-//     }
-// for (var i = 0; i < data.length; i++) {
-//     const temp = data.main.temp
-//     const wind = data.wind.speed;
-//     const humidity = data.main.humidity;
-//     console.log(temp, wind, humidity);
-//     return {
-//         temp: temp,
-//         wind: wind,
-//         humidity: humidity
-//     }
-// }
-// for (var i = 0; i < data.list.length; i++) {
-//     printResults(locRes.results[i]);//checking the time dt
-
-// function renderData(fiveDayArr) {
-//     data.forEach(fiveDayArr => {
-//         const div = document.createElement('div');
-//         const temp = document.createElement('h3');
-//         const wind = document.createElement('h3');
-//         const humidity = document.createElement('h3');
-
-//         div.classList = 'card'
-//         temp.innerText = `Temp: ${data.main.temp}`
-//         wind.innerText = `Wind: ${data.wind.speed}`
-//         humidity.innerText = `Humidity: ${data.main.humidity}`
-
-//         div.appendChild(temp)
-//         div.appendChild(wind)
-//         div.appendChild(humidity)
-//     });
-// };
-
-
-//  if dt... is 1:00 then get the data for that item for that specific time and render a forecast card for it (for loop)
-// function that creates html elements for each piece of weather data that we want
-
 //use js or jQuery to build the cards within the functions
 searchButton.addEventListener('click', onSearch)//when we click it will run the onSearch function
 
